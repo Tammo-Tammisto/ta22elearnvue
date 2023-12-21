bae658fc240e0f269d0aa80bfaaf8f3520a5b6b1
Tammo-Tammisto <113598068+Tammo-Tammisto@users.noreply.github.com>
2023-12-21 10:51:03 +0200

    Add files via upload

---
+++ src/components/Message.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref } from 'vue';
+    defineProps(['open']);
+</script>
+
+<template>
+    <article class="message is-success is-medium" v-if="open">
+        <div class="message-header">
+            <p>Message</p>
+            <button class="delete" @click="$emit('close')"></button>
+        </div>
+        <slot></slot>
+    </article>
+</template>
