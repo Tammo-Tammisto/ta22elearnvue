5ada50eb7703acc2ec0787013eaec0a159adae6f
Tammo-Tammisto <113598068+Tammo-Tammisto@users.noreply.github.com>
2023-12-21 10:51:24 +0200

    Add files via upload

---
+++ src/pages/Message.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from 'vue';
+    import Message from '../components/Message.vue'
+    let messageOpen = ref(false);
+</script>
+
+<template>
+    <button class="button is-primary" @click="messageOpen = true" v-if="messageOpen == false">Open message</button>
+    <button class="button is-primary" @click="messageOpen = false" v-if="messageOpen == true">Close message</button>
+    <Message :open="messageOpen" @close="messageOpen = false">
+      <div class="message-body">
+        Message body
+      </div>
+    </Message>
+</template>
